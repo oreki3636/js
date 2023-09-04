4def5517aa5e2e15b7b173f11794695e1c92fd45
oreki3636 <127170175+oreki3636@users.noreply.github.com>
2023-09-04 07:23:56 +0000

    Java Scriptを学ぼう6章完了

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-
-
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
 });
